f79dbc3340599a2271b257b9c21eba6076ed3a46
Tudor Scripca <scripcatudor1@gmail.com>
2023-06-16 13:02:38 +0100

    check if a letter belongs to the string.

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         const int MAX_TRIES = 5;
 
-        var hiddenWords = new List<string>()
+        List<string> hiddenWords = new List<string>()
         {
             "airplane",
             "parachute",
@@ -19,30 +19,28 @@ class Program
 
         Random rng = new Random();
         int randomPicker = rng.Next(hiddenWords.Count);
-
-        //Console.WriteLine(hiddenWords[randomPicker]);
+                
         string randomHiddenWord = hiddenWords[randomPicker];
-
-        //print the "_" for each letter in the random word that the program picked from the list of words
+                
         foreach (char letter in randomHiddenWord)
         {
             Console.Write("_");
         }
-        
-        //max.of tries is 5
+
         for (int guessCounter = 0; guessCounter < MAX_TRIES; guessCounter++)
         {
-           
-            Console.WriteLine("\nInsert letter: ");
-            string userLetter = Console.ReadLine();
-
             if (guessCounter >= MAX_TRIES)
             {
                 Console.WriteLine("GAME OVER!");
+                return;
             }
-            for (int i = randomHiddenWord.Length; i >= 0; i++)
+
+            Console.WriteLine("\nInsert letter: ");
+            char userLetter = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i <= randomHiddenWord.Length; i++)
             {
-                Console.Write(i);
+                Console.WriteLine();
             }
         }
     }
