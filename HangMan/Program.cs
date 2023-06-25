dd4cd4eb841d81a6feb948b1dbe8ee9a175245df
tscripca <scripcatudor1@gmail.com>
2023-06-25 06:48:22 +0100

    Store the underscores in a list and then swap them with the corrwect guessed letter. Ask user to play again.

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,46 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-class Program
+
+namespace HangMan // Note: actual namespace depends on the project name.
 {
-    static void Main()
+    internal class Program
     {
-        const int MAX_TRIES = 5;
+        static void Main(string[] args)
+        {            
+            const int MAX_TRIES = 4;
+            int guessCounter = 0;
+            char underline = '_';
+            char userLetter;
+            bool answer = true;
 
-        List<string> hiddenWords = new List<string>()
-        {
-            "airplane",
-            "parachute",
-            "framework",
-            "netherlands",
-            "sternocleidomastoid",
-            "carbonara",
-            "massachusetts"
-        };
+            while (answer)
+            {                
+                List<char> storeLetters = new List<char>();
+                List<string> hiddenWords = new List<string>()
+                {
+                "airplane",
+                "parachute",
+                "framework",
+                "netherlands",
+                "sternocleidomastoid",
+                "carbonara",
+                "massachusetts"
+                };
 
-        Random rng = new Random();
-        int randomPicker = rng.Next(hiddenWords.Count);
-                
-        string randomHiddenWord = hiddenWords[randomPicker];
+                Console.Clear();
 
-        for (int letterCheck = 0; letterCheck <= randomHiddenWord.Length; letterCheck++)
-        {
-            Console.Write("_");
-        }
-        
-        for (int guessCounter = 0; guessCounter < MAX_TRIES; guessCounter++)
-        {
-            if (guessCounter >= MAX_TRIES)
-            {
-                Console.WriteLine("GAME OVER!");
-                return;
-            }
+                Random rng = new Random();
+                int randomPicker = rng.Next(hiddenWords.Count);
+                string randomHiddenWord = hiddenWords[randomPicker];
 
-            Console.WriteLine("\nInsert letter: ");
-            char userLetter = Convert.ToChar(Console.ReadLine());
+                for (int j = 0; j < randomHiddenWord.Length; j++)
+                {
+                    storeLetters.Add(underline);
+                    Console.Write(storeLetters[j]);
+                }
 
-            for (int i = 0; i <= randomHiddenWord.Length; i++)
-            {
-                Console.WriteLine();
+                for (guessCounter = 0; guessCounter < MAX_TRIES; guessCounter++)
+                {
+                    Console.Write("\nInsert letter: ");
+                    userLetter = Convert.ToChar(Console.ReadLine());
+
+                    for (int i = 0; i < randomHiddenWord.Length; i++)
+                    {
+                        if (userLetter == randomHiddenWord[i])
+                        {
+                            storeLetters[i] = userLetter;//here is where the underline becomes the guessed letter
+                            Console.Write(storeLetters[i]);
+                        }                        
+                        else
+                        {
+                            Console.Write(storeLetters[i]);
+                        }                        
+                    }
+                }
+
+                if (guessCounter == MAX_TRIES)
+                {
+                    Console.WriteLine("\nGAME OVER!\nWould you like to play again?: Y/N");
+                    string userAnswer = Console.ReadLine().ToLower();
+                    answer = (userAnswer == "y");
+                }
             }
         }
     }
