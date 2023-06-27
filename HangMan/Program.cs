9015219cfeaa0878178bda33603e3ef2e0fb527f
tscripca <scripcatudor1@gmail.com>
2023-06-27 06:52:39 +0100

    Lines 32 and 33 moved from outside to inside the while(playAgain) Loop. Line 51 added information to the user on how many tries left. Lines 63 to77, ask user to guess the word on the last try.

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -5,17 +5,16 @@ namespace HangMan // Note: actual namespace depends on the project name.
     internal class Program
     {
         static void Main(string[] args)
-        {            
-            const int MAX_TRIES = 4;
+        {
+            const int MAX_TRIES = 3;
+            const int LAST_CHANCE = 2;
             int guessCounter = 0;
             char underline = '_';
             char userLetter;
-            bool answer = true;
+            bool playAgain = true;
 
-            while (answer)
-            {                
-                List<char> storeLetters = new List<char>();
-                List<string> hiddenWords = new List<string>()
+            List<char> guessedLetter = new List<char>();
+            List<string> hiddenWords = new List<string>()
                 {
                 "airplane",
                 "parachute",
@@ -26,43 +25,62 @@ namespace HangMan // Note: actual namespace depends on the project name.
                 "massachusetts"
                 };
 
-                Console.Clear();
+            Random rng = new Random();            
 
-                Random rng = new Random();
+            while (playAgain)
+            {
                 int randomPicker = rng.Next(hiddenWords.Count);
                 string randomHiddenWord = hiddenWords[randomPicker];
 
+                Console.Clear();
+
                 for (int j = 0; j < randomHiddenWord.Length; j++)
                 {
-                    storeLetters.Add(underline);
-                    Console.Write(storeLetters[j]);
+                    guessedLetter.Add(underline);
+                    Console.Write(guessedLetter[j]);
                 }
 
                 for (guessCounter = 0; guessCounter < MAX_TRIES; guessCounter++)
                 {
+                    int guessesLeft = MAX_TRIES - guessCounter;                    
+
                     Console.Write("\nInsert letter: ");
                     userLetter = Convert.ToChar(Console.ReadLine());
+                    
+                    Console.Clear();
+                    Console.Write($"\nyou have {guessesLeft} tries left.\n");
 
                     for (int i = 0; i < randomHiddenWord.Length; i++)
                     {
                         if (userLetter == randomHiddenWord[i])
                         {
-                            storeLetters[i] = userLetter;//here is where the underline becomes the guessed letter
-                            Console.Write(storeLetters[i]);
-                        }                        
+                            guessedLetter[i] = userLetter;//here is where the underline becomes the guessed letter                            
+                        }
+
+                        Console.Write(guessedLetter[i]);
+                    }
+
+                    if (guessCounter == LAST_CHANCE)
+                    {
+                        Console.WriteLine($"\nTry to guess the word: ");
+                        string userKnowsTheWord = Console.ReadLine();
+
+                        if (userKnowsTheWord == randomHiddenWord)
+                        {
+                            Console.WriteLine("WINNER!");                            
+                        }     
                         else
                         {
-                            Console.Write(storeLetters[i]);
-                        }                        
-                    }
+                            Console.WriteLine("\nSorry, GAME OVER!");
+                        }
+                        break;
+                    }                    
                 }
-
-                if (guessCounter == MAX_TRIES)
-                {
-                    Console.WriteLine("\nGAME OVER!\nWould you like to play again?: Y/N");
-                    string userAnswer = Console.ReadLine().ToLower();
-                    answer = (userAnswer == "y");
-                }
+                
+                Console.WriteLine("\nWould you like to play again?: Y/N");
+                string userAnswer = Console.ReadLine().ToLower();
+                playAgain = (userAnswer == "y");
+                guessedLetter.Clear();
             }
         }
     }
